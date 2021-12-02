375f20e556665dd0f61a8d99fb32a45cc6c76ff2
Paloma Brandao <brandao.pcg@gmail.com>
2021-12-01 22:50:32 -0600

    Implemented order placement through Active Admin.

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,7 @@ class Order < ApplicationRecord
   belongs_to :order_status
   has_many :products, through: :carts
   has_many :carts
+
+  accepts_nested_attributes_for :address, allow_destroy: true
+  accepts_nested_attributes_for :carts, allow_destroy: true
 end
