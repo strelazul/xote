a068f97adea4b955d6565459b54295566c09987a
Paloma Brandao <brandao.pcg@gmail.com>
2021-12-08 22:44:27 -0600

    Fixing Rubocop ofenses: Added an unique index to category's name. Removed trailing spaces.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_061540) do
+ActiveRecord::Schema.define(version: 2021_12_09_041400) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_12_02_061540) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "cities", force: :cascade do |t|
