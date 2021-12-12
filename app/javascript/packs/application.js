7c9aa25bbd685d0aeb77edbb996a53a6f87ef9d3
Paloma Brandao <brandao.pcg@gmail.com>
2021-12-11 18:07:29 -0600

    Fixed Dropdown not working properly.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,26 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@popperjs/core")
+
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
+import "@popperjs/core"
+
+import "bootstrap"
+import { Tooltip, Popover, Dropdown } from "bootstrap"
+require("../stylesheets/application.scss")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import { Tooltip, Popover } from "bootstrap"
-require("../stylesheets/application.scss")
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 document.addEventListener("turbolinks:load", () => {
-  // Both of these are from the Bootstrap 5 docs
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
       return new Tooltip(tooltipTriggerEl)
@@ -23,8 +31,9 @@ document.addEventListener("turbolinks:load", () => {
   var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
       return new Popover(popoverTriggerEl)
   })
-})
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+  var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+  var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+  })
+ })
